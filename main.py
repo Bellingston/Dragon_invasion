21efc010aec77aa3d3a5b37d8358ec7c0474ac5d
Bellingston <65080946+Bellingston@users.noreply.github.com>
2024-08-16 14:48:30 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,83 @@
+import pygame
+import sys
+
+from settings import Settings
+from dragon import Dragon
+
+
+class DragonInvasion:
+    '''Класс управления игрой'''
+    def __init__(self) -> None:
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption('Dragon Invasion')
+        self.dragon = Dragon(self)
+
+    def run_game(self):
+        '''Запуск цикла игры'''
+        while True:
+            self._check_events()
+            self.dragon.update()
+            self._update_screen()
+
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.dragon.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.dragon.moving_left = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.dragon.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.dragon.moving_left = False
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.dragon.blitme()
+        pygame.display.flip()
+
+
+
+if __name__ == '__main__':
+    di = DragonInvasion()
+    di.run_game()
+
+
+
+
+
+
+
+
+# def run_game():
+#     pygame.init()
+#     game_settings = Settings()
+#     screen = pygame.display.set_mode(
+#         (game_settings.screen_width, game_settings.screen_height)
+#         )
+#     pygame.display.set_caption("Dragon Invasion")
+
+#     while True:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 sys.exit()
+        
+#         screen.fill(game_settings.bg_color)
+
+#         pygame.display.flip()
+
+# run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    '''Класс хранения настроек игры'''
+
+    def __init__(self) -> None:
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_color = (27, 162, 240)
+        self.dragon_speed = 1.5
