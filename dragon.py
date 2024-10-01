c85024c6d0e04c6056d3b36b2d4ed8621e35bbac
Bellingston <65080946+Bellingston@users.noreply.github.com>
2024-10-02 00:49:20 +0300

    up and down control

--- dragon.py
+++ dragon.py
@@ -1,7 +1,9 @@
 import pygame
 
+
 class Dragon():
     '''Класс управления драконом'''
+
     def __init__(self, di_game) -> None:
         self.screen = di_game.screen
         self.settings = di_game.settings
@@ -14,18 +16,25 @@ class Dragon():
         self.rect.midbottom = self.screen_rect.midbottom
 
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.dragon_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.dragon_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.dragon_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.dragon_speed
 
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
-        
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pygame
 import sys
+import pygame
 
 from settings import Settings
 from dragon import Dragon
@@ -11,8 +11,11 @@ class DragonInvasion:
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
+            # (self.settings.screen_width, self.settings.screen_height)
+            (0, 0), pygame.FULLSCREEN
         )
+        self.settings.screen_height = self.screen.get_rect().height
+        self.settings.screen_width = self.screen.get_rect().width
         pygame.display.set_caption('Dragon Invasion')
         self.dragon = Dragon(self)
 
@@ -22,7 +25,6 @@ class DragonInvasion:
             self._check_events()
             self.dragon.update()
             self._update_screen()
-
 
     def _check_events(self):
         for event in pygame.event.get():
@@ -38,18 +40,27 @@ class DragonInvasion:
             self.dragon.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.dragon.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.dragon.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.dragon.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.dragon.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.dragon.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.dragon.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.dragon.moving_down = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.dragon.blitme()
         pygame.display.flip()
-
 
 
 if __name__ == '__main__':
